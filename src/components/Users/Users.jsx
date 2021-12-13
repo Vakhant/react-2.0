6240cacdb2fe58614bc8b130d46ea06c2d9f752c
VirSpectabilis <rinaldo124@mail.ru>
2021-12-14 01:57:56 +0300

    main: users.module.css

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,20 @@
+import css from './Users.module.css';
 
 const Users = (props) => {
   if(props.users.length === 0) {
     props.setUsers([
-      {id:"1", photoUrl: "img1", followed: true, name:"Max", status: "kek", location:{country: "Russia", city: "Spb"}},
-      {id:"2", photoUrl: "img2", followed: false, name:"Dmitry", status: "lol", location:{country: "Russia", city: "Moscow"}},
-      {id:"3", photoUrl: "img3", followed: true, name:"Axie", status: "cheburan", location:{country: "Russia", city: "Ekaterinburg"}},
-      {id:"4", photoUrl: "img4", followed: true, name:"Tvarina", status: "uran", location:{country: "Russia", city: "Novgorod"}},
+      {id:"1", photoUrl: "img1", followed: true, fullName: "Max", status: "kek", location:{country: "Russia", city: "Spb"}},
+      {id:"2", photoUrl: "img2", followed: false, fullName: "Dmitry", status: "lol", location:{country: "Russia", city: "Moscow"}},
+      {id:"3", photoUrl: "img3", followed: true, fullName: "Axie", status: "cheburan", location:{country: "Russia", city: "Ekaterinburg"}},
+      {id:"4", photoUrl: "img4", followed: true, fullName: "Tvarina", status: "uran", location:{country: "Russia", city: "Novgorod"}},
   
     ])
   }
   return (
-    <div>
+    <div className={css.user_list}>
       {
-        props.users.map( u => <div key={u.id}>
-            <div className="user_logo"></div>
-            {u.followed ? 
-            <button onClick={()=>props.unfollow(u.id)}>follow</button> :
-            <button onClick={()=>props.follow(u.id)}>unfollow</button>
-            }
+        props.users.map( u => <div className={css.user_block} key={u.id}>
+            <div className="user_logo">{u.photoUrl}</div>
             <div className="user_full_name">
               {u.fullName}
             </div>
@@ -28,6 +25,10 @@ const Users = (props) => {
               <div>{u.location.city}</div>
               <div>{u.location.country}</div>
             </div>
+            {u.followed ? 
+            <button onClick={()=>props.unfollow(u.id)}>follow</button> :
+            <button onClick={()=>props.follow(u.id)}>unfollow</button>
+            }
           </div>
         )
       }
