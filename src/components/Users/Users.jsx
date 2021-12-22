a134aabc054228bf8f1573e439c4009c7f99bf53
VirSpectabilis <rinaldo124@mail.ru>
2021-12-22 23:35:09 +0300

    main: users pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,16 +5,43 @@ import React from 'react';
 
 class Users extends React.Component{
 
+  // componentDidMount(){
+  //   axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+  //     this.props.setUsers(response.data.items)
+  //     this.props.setTotalUsersCount(response.data.totalCount)
+  //   })
+  // }
   getUsers = () => {
     if(this.props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
         this.props.setUsers(response.data.items)
+        this.props.setTotalUsersCount(response.data.totalCount)
       })
     }
   }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+      this.props.setUsers(response.data.items)
+    })
+  }
+
   render(){
+    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+    console.log(pages);
     return (
       <div className={css.user_list}>
+        <div>
+          {pages.map(p => {
+            return <span className={css.page_number + ` ${this.props.currentPage === p && css.selectedPage}`}
+            onClick={(e) => this.onPageChanged(p)}>{p}</span>
+          })}
+        </div>
       <button onClick={this.getUsers}>users list</button>
         {
           this.props.users.map( u => <div className={css.user_block} key={u.id}>
