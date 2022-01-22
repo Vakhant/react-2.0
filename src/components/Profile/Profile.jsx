a760fea629a51db977333fb80dbfd9914b18f0ea
VirSpectabilis <rinaldo124@mail.ru>
2022-01-23 00:04:15 +0300

    main: setstate, local state

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Preloader from '../common/Preloader/Preloader';
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import css from './Profile.module.css';
@@ -12,6 +13,38 @@ const ProfileInfo = (props) => {
   )
 }
 
+class ProfileStatus extends React.Component{
+  state = {
+    editMode: false,
+    status: 'fdfd'
+  }
+
+  activateEditMode = () => {
+    this.setState({
+      editMode: true
+    })
+  }
+
+  deactivateEditMode = () => {
+    this.setState({
+      editMode: false
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        {!this.state.editMode &&
+        <div onClick={this.activateEditMode.bind(this)}>{this.state.status}</div>
+        }
+        {this.state.editMode &&
+        <div autoFocus={true} onBlur={this.deactivateEditMode.bind(this)}><input value={this.state.status} type="text" /></div>
+        }
+      </div>
+    )
+  }
+}
+
 const Profile = (props) => {
   if(!props.profile){
     return <Preloader/>
@@ -19,6 +52,7 @@ const Profile = (props) => {
     return (
       <>
       <ProfileInfo profile={props.profile}/>
+      <ProfileStatus/>
       <MyPostsContainer store={props.store}/>
       </>
     );
